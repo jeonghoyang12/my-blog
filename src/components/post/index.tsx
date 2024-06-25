6eda10ffb836f607394da6ed34c65402b1b11ca6
jeonghoyang12 <wjdwjdgh6998@gmail.com>
2024-06-25 18:48:22 +0900

    chore: update notion style

--- src/components/post/index.tsx
+++ src/components/post/index.tsx
@@ -15,7 +15,7 @@ export function Post(props: PostProps) {
         <h1 className="text-4xl font-bold">{title}</h1>
         <p className="mt-2 text-gray-500">{formatDate(date)}</p>
         <div
-          className="prose prose-p:text-white prose-headings:text-white text-md mt-4 max-w-3xl leading-10"
+          className="notion-render prose prose-p:text-white prose-headings:text-white text-md mt-4 max-w-3xl leading-10"
           dangerouslySetInnerHTML={{ __html: content }}
         ></div>
       </div>
