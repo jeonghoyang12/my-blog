3dd7ab92e168d71c36091b8706434dea777b5261
jeonghoyang12 <wjdwjdgh6998@gmail.com>
2024-06-27 15:56:08 +0900

    chore: update next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,28 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  env: {
+    NOTION_API_KEY: process.env.NOTION_API_KEY,
+    NOTION_DATABASE_ID: process.env.NOTION_DATABASE_ID,
+  },
+  webpack: (config, { webpack }) => {
+    // Log the environment variables to verify they are set
+    console.log('NOTION_API_KEY:', process.env.NOTION_API_KEY)
+    console.log('NOTION_DATABASE_ID:', process.env.NOTION_DATABASE_ID)
+
+    // Define the environment variables for the client-side
+    config.plugins.push(
+      new webpack.DefinePlugin({
+        'process.env.NOTION_API_KEY': JSON.stringify(
+          process.env.NOTION_API_KEY
+        ),
+        'process.env.NOTION_DATABASE_ID': JSON.stringify(
+          process.env.NOTION_DATABASE_ID
+        ),
+      })
+    )
+
+    return config
+  },
+}
 
 export default nextConfig
