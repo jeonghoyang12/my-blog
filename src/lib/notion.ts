c57f0c03c57f0aa69c033c65e9aee371a6593ae8
jeonghoyang12 <wjdwjdgh6998@gmail.com>
2024-06-28 21:01:47 +0900

    chore: update api keys

--- src/lib/notion.ts
+++ src/lib/notion.ts
@@ -5,7 +5,10 @@ import type {
   PageObjectResponse,
 } from '@notionhq/client/build/src/api-endpoints'
 
-if (!process.env.NOTION_API_KEY || !process.env.NOTION_DATABASE_ID) {
+if (
+  !process.env.NEXT_PUBLIC_NOTION_API_KEY ||
+  !process.env.NEXT_PUBLIC_NOTION_DATABASE_ID
+) {
   console.error(
     'Missing required environment variables NOTION_API_KEY or NOTION_DATABASE_ID'
   )
@@ -14,16 +17,16 @@ if (!process.env.NOTION_API_KEY || !process.env.NOTION_DATABASE_ID) {
   )
 }
 
-console.log('NOTION_API_KEY:', process.env.NOTION_API_KEY)
-console.log('NOTION_DATABASE_ID:', process.env.NOTION_DATABASE_ID)
+console.log('NOTION_API_KEY:', process.env.NEXT_PUBLIC_NOTION_API_KEY)
+console.log('NOTION_DATABASE_ID:', process.env.NEXT_PUBLIC_NOTION_DATABASE_ID)
 
 export const notion = new Client({
-  auth: process.env.NOTION_API_KEY,
+  auth: process.env.NEXT_PUBLIC_NOTION_API_KEY,
 })
 
 export const fetchPages = React.cache(() => {
   return notion.databases.query({
-    database_id: process.env.NOTION_DATABASE_ID ?? '',
+    database_id: process.env.NEXT_PUBLIC_NOTION_DATABASE_ID ?? '',
     filter: {
       property: 'Published',
       checkbox: {
@@ -36,7 +39,7 @@ export const fetchPages = React.cache(() => {
 export const fetchPageBySlug = React.cache((slug: string) => {
   return notion.databases
     .query({
-      database_id: process.env.NOTION_DATABASE_ID ?? '',
+      database_id: process.env.NEXT_PUBLIC_NOTION_DATABASE_ID ?? '',
       filter: {
         property: 'Slug',
         rich_text: {
